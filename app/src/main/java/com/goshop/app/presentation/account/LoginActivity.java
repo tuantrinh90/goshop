44805a2be05cd7b18b91c2a2908b95b0c44dfd23
joyson <joysonchen@qq.com>
2018-01-09 10:32:41 +0800

    login ui

---
+++ app/src/main/java/com/goshop/app/presentation/account/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.goshop.app.presentation.account;
+
+import com.goshop.app.R;
+import com.goshop.app.base.BaseActivity;
+
+/**
+ * Created by img on 2018/1/8.
+ */
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    public int getContentView() {
+        return R.layout.account_login;
+    }
+
+    @Override
+    public String getScreenTitle() {
+        return null;
+    }
+
+    @Override
+    public void inject() {
+
+    }
+}
--- app/src/main/java/com/goshop/app/presentation/home/HomeActivity.java
+++ app/src/main/java/com/goshop/app/presentation/home/HomeActivity.java
@@ -1,20 +1,24 @@
 package com.goshop.app.presentation.home;
 
-import android.os.Bundle;
-import android.widget.Toast;
-
 import com.goshop.app.GoShopApplication;
 import com.goshop.app.R;
 import com.goshop.app.base.BaseActivity;
 import com.goshop.app.common.view.CustomBoldButton;
 import com.goshop.app.data.model.Weather;
+import com.goshop.app.presentation.account.LoginActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 import injection.components.DaggerPresenterComponent;
 import injection.modules.PresenterModule;
 
-public class HomeActivity extends BaseActivity<HomeContract.Presenter> implements HomeContract.View{
+public class HomeActivity extends BaseActivity<HomeContract.Presenter> implements HomeContract
+    .View {
 
     @BindView(R.id.tv_c)
     CustomBoldButton tvC;
@@ -26,27 +30,27 @@ public class HomeActivity extends BaseActivity<HomeContract.Presenter> implement
 
     @Override
     public void showWeather(Weather weather) {
-        Toast.makeText(this,"wather:"+weather.getTemp1(),Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "wather:" + weather.getTemp1(), Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void showNetwordErrorMessage() {
-        Toast.makeText(this,"netword error",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "netword error", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void showFaildMessage(String errorMessage) {
-        Toast.makeText(this,""+errorMessage,Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "" + errorMessage, Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void inject() {
         //init Presenter  use Dagger
         DaggerPresenterComponent.builder()
-                .applicationComponent(GoShopApplication.getApplicationComponent())
-                .presenterModule(new PresenterModule(this))
-                .build()
-                .inject(this);
+            .applicationComponent(GoShopApplication.getApplicationComponent())
+            .presenterModule(new PresenterModule(this))
+            .build()
+            .inject(this);
     }
 
     @Override
@@ -59,11 +63,17 @@ public class HomeActivity extends BaseActivity<HomeContract.Presenter> implement
         return null;
     }
 
-    @OnClick(R.id.tv_c)
-    public void onClick() {
-        mPresenter.getWeather();
-
+    @OnClick({R.id.tv_c, R.id.btn_click_login, R.id.tv_text})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_c:
+                mPresenter.getWeather();
+                break;
+            case R.id.btn_click_login:
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                break;
+            case R.id.tv_text:
+                break;
+        }
     }
-
-
 }
