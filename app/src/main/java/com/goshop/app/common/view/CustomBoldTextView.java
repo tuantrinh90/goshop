9ef8e7d96a1a26049f0037ba1cc406c16df25862
joyson <joysonchen@qq.com>
2018-01-18 14:59:10 +0800

    modify code comment

--- app/src/main/java/com/goshop/app/common/view/CustomBoldTextView.java
+++ app/src/main/java/com/goshop/app/common/view/CustomBoldTextView.java
@@ -1,10 +1,11 @@
 package com.goshop.app.common.view;
 
+import com.goshop.app.common.Typefaces;
+
 import android.content.Context;
 import android.graphics.Typeface;
 import android.text.TextPaint;
 import android.util.AttributeSet;
-import com.goshop.app.common.Typefaces;
 
 /**
  * Created by winniseptiani on 3/16/17.
@@ -15,17 +16,20 @@ public class CustomBoldTextView extends android.support.v7.widget.AppCompatTextV
 
     public CustomBoldTextView(Context context) {
         super(context);
-        setFont(context);
+//TODO if have font file open this method
+//        setFont(context);
     }
 
     public CustomBoldTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setFont(context);
+//TODO if have font file open this method
+//        setFont(context);
     }
 
     public CustomBoldTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setFont(context);
+//TODO if have font file open this method
+//        setFont(context);
     }
 
     private void setFont(Context context) {
