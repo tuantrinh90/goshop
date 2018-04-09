6f45e0d0db4a58b4d72e3e2977f8b216ab2a75ce
helen <helen.he@imaginato.com>
2018-04-09 16:29:37 +0800

    fix a bug

--- app/src/main/java/com/goshop/app/data/model/response/common/PriceData.java
+++ app/src/main/java/com/goshop/app/data/model/response/common/PriceData.java
@@ -1,11 +1,13 @@
 package com.goshop.app.data.model.response.common;
 
+import com.google.gson.annotations.SerializedName;
+
 public class PriceData {
 
     /**
      * RM : {"original":"200","discounted":"149","discount_title":"25% OFF"}
      */
-
+    @SerializedName("RM")
     private RMData rm;
 
     public RMData getRM() {
