896387203221ae3341cea9c46b3667864558377b
helen <helen.he@imaginato.com>
2018-05-15 14:10:56 +0800

    delete file by comment and update gradle

--- app/src/main/java/com/goshop/app/presentation/pushwoosh/MyFirebaseMessagingService.java
+++ app/src/main/java/com/goshop/app/presentation/pushwoosh/MyFirebaseMessagingService.java
@@ -3,7 +3,6 @@ package com.goshop.app.presentation.pushwoosh;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
@@ -13,46 +12,21 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      *
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
-    // [START receive_message]
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // [START_EXCLUDE]
-        // There are two types of messages data messages and notification messages. Data messages
-        // are handled
-        // here in onMessageReceived whether the app is in the foreground or background. Data
-        // messages are the type
-        // traditionally used with GCM. Notification messages are only received here in
-        // onMessageReceived when the app
-        // is in the foreground. When the app is in the background an automatically generated
-        // notification is displayed.
-        // When the user taps on the notification they are returned to the app. Messages
-        // containing both notification
-        // and data payloads are treated as notification messages. The Firebase console always
-        // sends notification
-        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
 
         // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         //todo this will use later and then delete please dont remove now
         remoteMessage.getFrom();
-
-        // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             //todo this will use later and then delete please dont remove now
             remoteMessage.getData();
         }
-
-        // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             //todo this will use later and then delete please dont remove now
             remoteMessage.getNotification().getBody();
         }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
-    // [END receive_message]
 
 
 }
